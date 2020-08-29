8948d3ee2ed82d8febd7bb353a02d5790850ee24
divyadarshini11 <divyamadhavan11@gmail.com>
2020-08-29 22:19:34 +0530

    First commit

---
+++ src.js
@@ -0,0 +1,35 @@
+function myfunction()
+{
+    var text ;
+    var item_name = prompt("ENTER ITEM NAME: ", "Enter Item name");
+    switch(item_name)
+    {
+        case "Green Moong":
+            text = "Item Name: "+item_name+ " || ID = 1C2540 || RATE = Rs. 100.46/kg";
+            text.fontsize("10");
+            break;
+        case "Urad Dal":
+            text = "Item Name: "+item_name+ " || ID = 1F9872 || RATE = Rs. 104.25/kg";
+            break;
+        case "Masoor Dal":
+            text = "Item Name: "+item_name+ " || ID = 1D8410 || RATE = Rs. 155.00/kg";
+            break;
+        case "Toor Dal":
+            text = "Item Name: "+item_name+ " || ID = 1E6026 || RATE = Rs. 90.99/kg";
+            break;
+        case "Cheakpea":
+            text = "Item Name: "+item_name+ " || ID = 1A7994 || RATE = Rs. 80.75/kg";
+            break;
+        case "Horsegram":
+            text = "Item Name: "+item_name+ " || ID = 1Q8345 || RATE = Rs. 105.50/kg";
+            break;
+        case "Matar Dal":
+           text = "Item Name: "+item_name+ " || ID = 1Z4349 || RATE = Rs. 120.45/kg";
+            break;
+        default:
+            text = "WRONG !! Please enter a valid commodity name......!!"
+    }
+    document.getElementById("demo").innerHTML = text;
+    document.getElementById("demo").style.textAlign = "center";
+    document.getElementById("demo").style.fontSize = "x-large";
+}
